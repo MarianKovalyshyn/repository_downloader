a0da478e6226ff2b5597c216585301dd1e763f84
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2024-01-17 13:06:34 +0200

    Fix not correct behavior with cached files

--- main.py
+++ main.py
@@ -62,74 +62,6 @@ async def login(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     return PROJECT_NAME
 
 
-def find_cached_project(repository_name: str, username: str) -> str | None:
-    local_project = glob.glob(f"{repository_name}[[]{username}[]]*.zip")
-    return local_project[0] if local_project else None
-
-
-def download_project(file_name, project_url: str) -> (str, bool):
-    open(file_name, "wb").write(requests.get(project_url).content)
-    return file_name, False
-
-
-def extract_project_date(project_to_send: str) -> date:
-    local_project_date = project_to_send[
-        project_to_send.find("(") + 1: project_to_send.find(")")
-    ]
-    local_project_date_object = datetime.strptime(
-        local_project_date, "%Y-%m-%d"
-    ).date()
-
-    return local_project_date_object
-
-
-def update_project_if_needed(
-    project_to_send: str, project_date: date, file_name: str
-):
-    if (date.today() - project_date).days > 7:
-        os.remove(project_to_send)
-        project_to_send, used_cached_file = download_project(
-            file_name, FINAL_URL
-        )
-        return project_to_send, used_cached_file
-
-
-def prepare_project_to_send(
-    repository_name: str,
-    repository_url: str,
-    username: str,
-    file_name: str = None,
-) -> (str, bool):
-    project_to_send = find_cached_project(repository_name, username)
-
-    if not project_to_send:
-        project_to_send, used_cached_file = download_project(
-            file_name, repository_url
-        )
-    else:
-        project_date = extract_project_date(project_to_send)
-        project_to_send, used_cached_file = update_project_if_needed(
-            project_to_send=project_to_send,
-            project_date=project_date,
-            file_name=file_name,
-        )
-
-    log_action(REPO_NAME, USERNAME, used_cached_file)
-
-    return project_to_send
-
-
-def log_action(
-    repository_name: str, username: str, used_cached_file: bool = False
-) -> None:
-    with open("logs.txt", "a") as file:
-        cached_or_downloaded = "cached" if used_cached_file else "downloaded"
-        file.write(
-            f"Project {repository_name} from {username}`s repository was send "
-            f"on {datetime.now()} ({cached_or_downloaded})\n"
-        )
-
-
 async def project_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global FINAL_URL, REPO_NAME
     REPO_NAME = update.message.text
@@ -160,6 +92,76 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     )
 
     return ConversationHandler.END
+
+
+def find_cached_project(repository_name: str, username: str) -> str | None:
+    local_project = glob.glob(f"{repository_name}[[]{username}[]]*.zip")
+    return local_project[0] if local_project else None
+
+
+def download_project(file_name, project_url: str) -> (str, bool):
+    open(file_name, "wb").write(requests.get(project_url).content)
+    return file_name, False
+
+
+def extract_project_date(project_to_send: str) -> date:
+    local_project_date = project_to_send[
+        project_to_send.find("(") + 1: project_to_send.find(")")
+    ]
+    local_project_date_object = datetime.strptime(
+        local_project_date, "%Y-%m-%d"
+    ).date()
+
+    return local_project_date_object
+
+
+def update_project_if_needed(
+    project_to_send: str, project_date: date, file_name: str, used_cached_file: bool
+):
+    if (date.today() - project_date).days > 7:
+        os.remove(project_to_send)
+        project_to_send, used_cached_file = download_project(
+            file_name, FINAL_URL
+        )
+
+    return project_to_send, used_cached_file
+
+
+def prepare_project_to_send(
+    repository_name: str,
+    repository_url: str,
+    username: str,
+    file_name: str = None,
+) -> (str, bool):
+    project_to_send = find_cached_project(repository_name, username)
+
+    if not project_to_send:
+        project_to_send, used_cached_file = download_project(
+            file_name, repository_url
+        )
+    else:
+        project_date = extract_project_date(project_to_send)
+        project_to_send, used_cached_file = update_project_if_needed(
+            project_to_send=project_to_send,
+            project_date=project_date,
+            file_name=file_name,
+            used_cached_file=True,
+        )
+
+    log_action(REPO_NAME, USERNAME, used_cached_file)
+
+    return project_to_send
+
+
+def log_action(
+    repository_name: str, username: str, used_cached_file: bool = False
+) -> None:
+    with open("logs.txt", "a") as file:
+        cached_or_downloaded = "cached" if used_cached_file else "downloaded"
+        file.write(
+            f"Project {repository_name} from {username}`s repository was send "
+            f"on {datetime.now()} ({cached_or_downloaded})\n"
+        )
 
 
 if __name__ == "__main__":
